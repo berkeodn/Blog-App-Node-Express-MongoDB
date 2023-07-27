f55ba17dab4d423dcb6a9b4b9c23430e9984311f
berkeodn <berkeoden@hotmail.com>
2023-07-27 21:53:18 +0300

    port is changed 5000 to 5001

--- index.js
+++ index.js
@@ -55,10 +55,10 @@ app.use('/searchedContents', async (req, res) => {
     }
   });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 db()
 
 app.listen(PORT, () => {
-    console.log("server is running on port: 5000");
+    console.log("server is running on port: 5001");
 })

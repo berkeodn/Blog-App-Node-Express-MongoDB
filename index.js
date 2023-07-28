f822db0aecab746034765feeff7d34b5d03a3fbf
berkeodn <berkeoden@hotmail.com>
2023-07-28 10:12:50 +0300

    timeline text wrapping is done

--- index.js
+++ index.js
@@ -55,10 +55,10 @@ app.use('/searchedContents', async (req, res) => {
     }
   });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 db()
 
 app.listen(PORT, () => {
-    console.log("server is running on port: 5001");
+    console.log("server is running on port: 5000");
 })
